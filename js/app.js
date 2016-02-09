8721adedd6a0da328fafeff6b162fe4bcb2382f9
Julia Sun <julia.sun@berkeley.edu>
2016-02-09 14:05:23 -0800

    added more cards

--- js/app.js
+++ js/app.js
@@ -2,9 +2,20 @@ var app = angular.module('valentines', [])
     .controller('MainCtrl', ['$scope', function ($scope) {
 
         $scope.cards = [
+            './img/cards/art.png',
+            './img/cards/apeeling.png',
             './img/cards/avocuddle.jpg',
             './img/cards/oliveyou.jpg',
-            './img/cards/pear.jpg'
+            './img/cards/backpack.png',
+            './img/cards/berry.png',
+            './img/cards/color.png',
+            './img/cards/egg.png',
+            './img/cards/filters.png',
+            './img/cards/hand.png',
+            './img/cards/pear.jpg',
+            './img/cards/pixel.png',
+            './img/cards/spark.png',
+            './img/cards/thang.png'
         ];
 
     }]);
