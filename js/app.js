ccc981ed8ebfbfa0eefc7972bac7cb0ab7aa9359
Julia Sun <julia.sun@berkeley.edu>
2016-02-10 11:20:54 -0800

    all valentines up!

--- js/app.js
+++ js/app.js
@@ -3,24 +3,28 @@ var app = angular.module('valentines', [])
 
         $scope.cards = [
             './img/cards/apeeling.png',
+            './img/cards/avocuddle.png',
             './img/cards/butter.png',
             './img/cards/berry.png',
             './img/cards/egg.png',
-            './img/cards/avocuddle.png',
-            './img/cards/donut.jpg',
-            './img/cards/backpack.png',
             './img/cards/filters.png',
-            './img/cards/koala.png',
-            './img/cards/latte.png',
-            './img/cards/art.png',
+            './img/cards/donut.jpg',
             './img/cards/hand.png',
+            './img/cards/backpack.png',
+            './img/cards/jar.png',
+            './img/cards/latte.png',
+            './img/cards/koala.png',
+            './img/cards/cheese.jpg',
             './img/cards/mango.png',
-            './img/cards/color.png',
+            './img/cards/art.png',
+            './img/cards/dimsum.png',
             './img/cards/pear.png',
-            './img/cards/pixel.png',
+            './img/cards/matcha.jpg',
             './img/cards/spark.png',
             './img/cards/thang.png',
             './img/cards/olive.png',
+            './img/cards/color.png',
+            './img/cards/pixel.png'
         ];
 
     }]);
